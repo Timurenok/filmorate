edbf92e569cbb8908a717ccb0be473be695f27b0
Timur <timurminaev07@gmail.com>
2023-08-26 14:30:10 +0300

    ФП10

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -5,9 +5,11 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
-
     void postFilm(Film film);
+
     void putFilm(Film film);
+
     List<Film> getFilms();
+
     Film getFilm(int id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -5,9 +5,11 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserStorage {
-
     void postUser(User user);
+
     void putUser(User user);
+
     List<User> getUsers();
+
     User getUser(int id);
 }
